fc1d06bf0fc9f2d25363734f929228469cc299c1
青青草原我最大 <71130464+dzmazhuo@users.noreply.github.com>
2023-05-16 16:39:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -5,7 +5,7 @@
  * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
  * 自行添加属性，以支持更多个性化功能
  */
-const USER_CONFIG = {"APP_ID":"wx403546cb0b6380ca","APP_SECRET":"45c64032e28433b2b0139691c2fb6ad7","IS_SHOW_COLOR":true,"CALLBACK_TEMPLATE_ID":"","CALLBACK_USERS":[{"name":"自己","id":"ov-Ve5hR1MRCojwF5oqYmSg1tp8M"}],"USERS":[{"name":"小叶","id":"ov-Ve5t-9tP70IjLrx5Qx0W4Ow4k","useTemplateId":"HAdGx8HX2iNrdEnektaw3AmIwNz0s1fwQe13yUXRLUM","province":"河南省","city":"南阳市","horoscopeDate":"03-14","horoscopeDateType":"今日","openUrl":"https://shuangxunian.github.io/","festivals":[{"type":"节日","name":"添加的时间","date":"05-16","year":"2023"},{"type":"*生日","name":"生日","date":"03-14","year":"2024"}],"customizedDateList":[{"keyword":"添加的日期","date":"2023-05-17"}]}],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":true,"oneTalk":true,"earthyLoveWords":false,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":true,"birthdayMessage":true}}
+const USER_CONFIG = {"APP_ID":"wx403546cb0b6380ca","APP_SECRET":"45c64032e28433b2b0139691c2fb6ad7","IS_SHOW_COLOR":true,"CALLBACK_TEMPLATE_ID":"","CALLBACK_USERS":[{"name":"自己","id":"ov-Ve5hR1MRCojwF5oqYmSg1tp8M"}],"USERS":[{"name":"小叶","id":"ov-Ve5t-9tP70IjLrx5Qx0W4Ow4k","useTemplateId":"HAdGx8HX2iNrdEnektaw3AmIwNz0s1fwQe13yUXRLUM","province":"河南省","city":"南阳市","horoscopeDate":"03-14","horoscopeDateType":"今日","openUrl":"https://shuangxunian.github.io/","festivals":[{"type":"节日","name":"星期天","date":"05-20","year":"2023"}],"customizedDateList":[{"keyword":"认识的日期","date":"2021-09-18"}]}],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":true,"oneTalk":true,"earthyLoveWords":false,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":true,"birthdayMessage":true}}
 
 module.exports = USER_CONFIG
 
